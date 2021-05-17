40a3efc31e75be00bac622a0c878c567801d6ff0
Carlos Garcia <carloseduardo.garciadelgado@soprasteria.com>
2021-05-17 20:31:37 +0200

    First commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from 'src/environments/environment';
+import { AppComponent } from './app.component';
+import { appReducers } from './app.reducer';
+import { FooterComponent } from './footer/footer.component';
+import { TodosModule } from './todos/todos.module';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    TodosModule,
+    StoreModule.forRoot(appReducers),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production, // Restrict extension to log-only mode
+    }),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
